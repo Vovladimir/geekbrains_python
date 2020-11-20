25c8357e63ae4837c2b04297b6d0e815503c16ad
Vladimir <vovladimir0@gmail.com>
2020-11-20 18:49:32 +0300

    задание 3

---
+++ hw3.py
@@ -0,0 +1,37 @@
+'''
+Реализовать базовый класс Worker (работник),
+в котором определить атрибуты: name, surname, position (должность), income (доход).
+Последний атрибут должен быть защищенным и ссылаться на словарь,
+содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}.
+Создать класс Position (должность) на базе класса Worker.
+В классе Position реализовать методы получения полного имени сотрудника (get_full_name)
+и дохода с учетом премии (get_total_income).
+Проверить работу примера на реальных данных
+(создать экземпляры класса Position, передать данные, проверить значения атрибутов, вызвать методы экземпляров).
+
+'''
+
+
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {'wage': wage, 'bonus': bonus}
+
+
+class Position(Worker):
+    def get_full_name(self):
+        return f'Полное имя сотрудника: {self.name} {self.surname}.'
+
+    def get_total_income(self):
+        return f'Доход с учётом премии = {self._income["wage"] + self._income["bonus"]}.'
+
+
+worker1 = Position('Vasya', 'Pupkin', 'manager', 10000, 2000)
+worker2 = Position('John', 'Smith', 'boss', 1000000, 200000)
+
+print(worker1.get_full_name())
+print(worker1.get_total_income())
+print(worker2.get_full_name())
+print(worker2.get_total_income())
